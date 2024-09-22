2345d5ad72244a4b0a66c2cde98718d70b66580d
R. Elliott Childre <elliottchildre329@gmail.com>
2024-09-22 10:52:24 -0400

    Address to_string_trait_impl lint for Status enum

--- src/cargo_ops/pkg_status.rs
+++ src/cargo_ops/pkg_status.rs
@@ -24,12 +24,12 @@ impl Status {
     pub fn is_changed(&self) -> bool { !matches!(*self, Status::Unchanged) }
 }
 
-impl ::std::string::ToString for Status {
-    fn to_string(&self) -> String {
+impl ::std::fmt::Display for Status {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match *self {
-            Status::Unchanged => "---".to_owned(),
-            Status::Removed => "Removed".to_owned(),
-            Status::Version(ref v) => v.to_string(),
+            Status::Unchanged => write!(f, "---"),
+            Status::Removed => write!(f, "Removed"),
+            Status::Version(ref v) => write!(f, "{}", v),
         }
     }
 }
